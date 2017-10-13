01cbcf22080c44ba319b497b3c15992a0730e64f
Harish <hthuwal@gmail.com>
2017-10-13 17:13:01 +0530

    Add check_sig

--- 04 Representing_Terms_Substitutions_and_Unification.ml
+++ 04 Representing_Terms_Substitutions_and_Unification.ml
@@ -4,3 +4,19 @@ type term = V of variable | Node of symbol * term list;;
 type arity = int;;
 type signature = (symbol * arity) list;;
 
+
+(* check_sig : Given a signature consisting of symbols and their arities (>= 0)
+it checks whether the signature is a valid signature (no repeated symbols,
+arities are non-negative etc. *)
+
+let check_sig sign =
+	(* Checking atleast one element with 0 arity *)
+	List.exists (fun x -> if snd x == 0 then true else false) sign &&
+	(* Checking no one element with negative arity *)
+	not (List.exists (fun x -> if snd x < 0 then true else false) sign) &&
+	(* Checking no repeated symbols *)
+	let rec no_duplicates s old_s = match s with
+		[] -> true
+		|x::xs -> if List.mem (fst x) old_s then false
+				  else no_duplicates xs ((fst x)::old_s) in
+	no_duplicates sign [];;
