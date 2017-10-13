22a67e6c26e9853ad124bc7a81a35c651bbd68bb
Harish <hthuwal@gmail.com>
2017-10-13 22:34:53 +0530

    Make compose tail recursive

--- 04 Representing_Terms_Substitutions_and_Unification.ml
+++ 04 Representing_Terms_Substitutions_and_Unification.ml
@@ -91,5 +91,4 @@ let rec subst (t:term) (sub:substitution) = match t with
 
 
 (* compose : composition of two substitutions returns a subtitution *)
-let rec compose sub1 sub2 =  (List.map ((fun sub lone_sub -> (fst lone_sub, subst (snd lone_sub) sub)) sub2) sub1) @ sub2 ;;
-	let 
+let rec compose sub1 sub2 =  sub2 @ (List.map ((fun sub lone_sub -> (fst lone_sub, subst (snd lone_sub) sub)) sub2) sub1);;
