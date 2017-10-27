4802d5bde6576ca155e1627c0bdaac368c9fd482
Harish <hthuwal@gmail.com>
2017-10-27 18:00:31 +0530

    Minor test case printing changes

--- 05 tester.ml
+++ 05 tester.ml
@@ -15,8 +15,8 @@ let prog1 = [Fact f2; Fact f3; Rule r1];;
 (* Goal *)
 let g1 = [f1; P "s4"];;
 
-Printf.printf "%B\n" (solve_dfs g1 prog1);;
-Printf.printf "%B\n" (solve_bfs g1 prog1);;
+Printf.printf "%i\n" (solve_dfs g1 prog1);;
+Printf.printf "%i\n" (solve_bfs g1 prog1);;
 
 (* Another set of test cases *)
 let fs1 = Fact (P "s");;
@@ -35,23 +35,23 @@ let gs1 = [P "s"; P "q"];;
 let gs2 = [P "z"];;
 
 (* Should return true *)
-Printf.printf "%B\n" (solve_dfs gs1 ps1);;
-Printf.printf "%B\n" (solve_bfs gs1 ps1);;
+Printf.printf "%i\n" (solve_dfs gs1 ps1);;
+Printf.printf "%i\n" (solve_bfs gs1 ps1);;
 
 
 (* Should return false *)
-Printf.printf "%B\n" (solve_dfs gs2 ps1);;
-Printf.printf "%B\n" (solve_bfs gs2 ps1);;
+Printf.printf "%i\n" (solve_dfs gs2 ps1);;
+Printf.printf "%i\n" (solve_bfs gs2 ps1);;
 
 
 (* Should return true *)
-Printf.printf "%B\n" (solve_dfs [] []);;
-Printf.printf "%B\n" (solve_bfs [] []);;
+Printf.printf "%i\n" (solve_dfs [] []);;
+Printf.printf "%i\n" (solve_bfs [] []);;
 
-Printf.printf "%B\n" (solve_dfs [] ps1);;
-Printf.printf "%B\n" (solve_bfs [] ps1);;
+Printf.printf "%i\n" (solve_dfs [] ps1);;
+Printf.printf "%i\n" (solve_bfs [] ps1);;
 
 
 (* Should return false *)
-Printf.printf "%B\n" (solve_dfs gs1 []);;
-Printf.printf "%B\n" (solve_bfs gs1 []);;
+Printf.printf "%i\n" (solve_dfs gs1 []);;
+Printf.printf "%i\n" (solve_bfs gs1 []);;
