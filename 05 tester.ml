723ea705d29f6e1008af522448619cb7dc2f4020
Harish <hthuwal@gmail.com>
2017-10-27 12:18:47 +0530

    Change test cases because of datatype modification

--- 05 tester.ml
+++ 05 tester.ml
@@ -1,16 +1,21 @@
 #use "05 Baby Propositional Prolog interpreter in OCaml.ml"
 
+(* Atoms *)
+let p1 = P "s1";;
+let p2 = P "s2";;
+let p3 = P "s3";;
+
 (* Facts *)
-let f1 = P "s1";;
-let f2 = P "s2";;
-let f3 = P "s3";;
+let f1 : atom * atom list = (P "s1",[]);;
+let f2 : atom * atom list = (P "s2",[]);;
+let f3 : atom * atom list = (P "s3",[]);;
 
 (* Rules *)
-let r1 = (f1, [f2;f3]);;
-let r2 = (f2, [f3;f1]);;
+let r1 = (p1, [p2;p3]);;
+let r2 = (p2, [p3;p1]);;
 
 (* Program *)
-let p1 = [Fact f2; Fact f3; Rule r1];;
+let p1 = [f2; f3; r1];;
 
 (* Goal *)
 let g1 = [f1];;
